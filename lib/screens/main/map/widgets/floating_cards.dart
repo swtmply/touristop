fa8bc0e4f0de87c6c7abba58dd5bacf8614a2e82
swtmply <delosreyesjohnallen@gmail.com>
2022-05-15 20:27:36 +0800

    updated floating cards

--- lib/screens/main/map/widgets/floating_cards.dart
+++ lib/screens/main/map/widgets/floating_cards.dart
@@ -19,47 +19,46 @@ class FloatingCards extends StatelessWidget {
         child: PageView.builder(
           controller: pageController,
           itemCount: spots.length,
-          itemBuilder: (context, index) =>
-              _spotsList(index, spots[index], pageController),
+          itemBuilder: (context, index) => _spotsList(index, spots[index]),
         ),
       ),
     );
   }
-}
 
-_spotsList(int index, TouristSpot spot, PageController pageController) {
-  return AnimatedBuilder(
-    animation: pageController,
-    builder: (context, child) {
-      double value = 1;
-      if (pageController.position.haveDimensions) {
-        value = pageController.page! - index;
-        value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
-      }
-      return Center(
-        child: SizedBox(
-          height: Curves.easeInOut.transform(value) * 130.0,
-          width: Curves.easeInOut.transform(value) * 350.0,
-          child: child,
-        ),
-      );
-    },
-    child: InkWell(
-      onTap: () {
-        // _createPolylines(userPosition, destination);
-        // _animateToSpot(position);
+  _spotsList(int index, TouristSpot spot) {
+    return AnimatedBuilder(
+      animation: pageController,
+      builder: (context, child) {
+        double value = 1;
+        if (pageController.position.haveDimensions) {
+          value = pageController.page! - index;
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 130.0,
+            width: Curves.easeInOut.transform(value) * 350.0,
+            child: child,
+          ),
+        );
       },
-      child: Container(
-        margin: const EdgeInsets.symmetric(
-          horizontal: 5,
+      child: InkWell(
+        onTap: () {
+          // _createPolylines(userPosition, destination);
+          // _animateToSpot(position);
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 5,
+          ),
+          height: 150.0,
+          width: 300.0,
+          color: Colors.white,
+          // ignore: todo
+          // TODO floating card component
+          child: Text(spot.name.toString()),
         ),
-        height: 150.0,
-        width: 300.0,
-        color: Colors.white,
-        // ignore: todo
-        // TODO floating card component
-        child: Text(spot.name.toString()),
       ),
-    ),
-  );
+    );
+  }
 }
